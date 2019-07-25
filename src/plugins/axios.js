5eee6f0e02d125a53da902390563895bba40e50b
laodingdang <13269735167@163.com>
2019-07-25 16:08:12 +0800

    封装axios插件 统一管理http方法

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || "";
@@ -6,9 +7,9 @@ import axios from "axios";
 // axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+  baseURL: process.env.baseURL || process.env.apiUrl || "",
+  timeout: 5000, // Timeout
+  withCredentials: true // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
@@ -35,3 +36,34 @@ _axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+function getAxios(method, url, params) {
+  let httpDefault = {
+    method: method,
+    url: url,
+    // params 是即将与请求一起发送的url参数
+    // data 是作为请求主体被发送的数据
+    params: method === "GET" || method === "DELETE" ? params : null,
+    data: method === "POST" || method === "PUT" ? qs.stringify(params) : null
+  };
+  return new Promise((resolve, reject) => {
+    _axios(httpDefault)
+      .then(res => {
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+// 输出函数get post put delect，供其他文件调用
+// Vue.js的插件应当又一个公开方法install，这个方法的第一个参数是Vue构造器，第二个参数是一个可选的vue对象
+export default {
+  install: Vue => {
+    Vue.prototype.getHttp = (url, params) => getAxios("GET", url, params);
+    Vue.prototype.postHttp = (url, params) => getAxios("POST", url, params);
+    Vue.prototype.putHttp = (url, params) => getAxios("PUT", url, params);
+    Vue.prototype.deleteHttp = (url, params) => getAxios("DELETE", url, params);
+  }
+};
