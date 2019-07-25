ba118694ed4d2461d38c8f2d5c1ba0e4dfc6af8c
laodingdang <13269735167@163.com>
2019-07-25 16:24:16 +0800

    add new page

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // 基本路径
-  baseUrl: "./",
+  publicPath: "./",
   // 输出文件目录
   outputDir: "dist",
   // eslint-loader 是否在保存的时候检查
   lintOnSave: true,
   // webpack配置
-  chainWebpack: () => { },
-  configureWebpack: () => { },
+  chainWebpack: () => {},
+  configureWebpack: () => {},
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: true,
   // css相关配置
@@ -30,18 +30,19 @@ module.exports = {
     port: 8080, // 端口号
     https: false,
     hotOnly: false, // https:{type:Boolean}
-    proxy: { // 配置跨域
-      "/api":{
-        target:"http://xxx.xx.xxx.xxx:8080", //源地址
-        changeOrigin:true,//改变源
-        ws:true, //是否代理websockets
-        pathRewrite:{
-          "^/api":""
+    proxy: {
+      // 配置跨域
+      "/api": {
+        target: "http://xxx.xx.xxx.xxx:8080", //源地址
+        changeOrigin: true, //改变源
+        ws: true, //是否代理websockets
+        pathRewrite: {
+          "^/api": ""
         }
       }
-    }, // 配置跨域处理,只有一个代理
-    before: app => {}
+    } // 配置跨域处理,只有一个代理
+    // before: app => {}
   },
   // 第三方插件配置
   pluginOptions: {}
-}
+};
